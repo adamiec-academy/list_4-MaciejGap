11351bd25ff2640937b0e3a0e38e0d23f6d00902
MaciejGap <115427042+MaciejGap@users.noreply.github.com>
2022-12-14 15:46:56 +0100

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,2 +1,9 @@
 def my_split(text):
+    word = ""
+    result = []
+    for character in data:
+        if character != " ":
+            word = word + character
+        elif character = " ":
+            result.append(word) 
     return [text]
